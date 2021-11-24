7854f543fec239de3f8e57b70b1686dea42dcf67
Bisio98 <alberto.bisetti@hotmail.it>
2021-11-24 14:00:03 +0100

    completed and corrected

--- js/script.js
+++ js/script.js
@@ -8,11 +8,11 @@ function start() {
     let numOfBoxes;
 
     if(difficulty === 'Easy'){
-        numOfBoxes = 49;
+        numOfBoxes = 100;
     }else if( difficulty === 'Medium'){
         numOfBoxes = 81;
     }else{
-        numOfBoxes = 100;
+        numOfBoxes = 49;
     }
 
     for(let i = 1; i <= numOfBoxes; i++) {
@@ -23,7 +23,7 @@ function start() {
         document.getElementById('box_game').appendChild(divBox);
         divBox.innerHTML = `<span>${i}</span>`;
 
-        if(numOfBoxes === 49){
+        if(numOfBoxes === 100){
             divBox.classList.add('easy');
         }else if( numOfBoxes === 81){
             divBox.classList.add('medium');
